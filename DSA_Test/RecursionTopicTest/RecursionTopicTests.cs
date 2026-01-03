43221adf54bec0cf14f352d98771a4dd019c2864
flinn <nhatlinhpt2015@gmail.com>
2026-01-03 15:36:00 +0700

    init project, learn basic recursion

---
+++ DSA_Test/RecursionTopicTest/RecursionTopicTests.cs
@@ -0,0 +1,93 @@
+﻿using MyApp.Console.RecursionTopic;
+
+namespace DSA_Test.RecursionTopicTest
+{
+    public class RecursionTopicTests
+    {
+        public RecursionTopicTests()
+        {
+            // Ensure test isolation
+            RecursionTopic.memo.Clear();
+        }
+
+        // ===================== FACTORIAL =====================
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 1)]
+        [InlineData(5, 120)]
+        [InlineData(10, 3628800)]
+        public void CalculateFactorial_GivenValidNumber_WhenCalled_ThenReturnsCorrectFactorial(
+            int input, long expected)
+        {
+            // Arrange (Given)
+
+            // Act (When)
+            var result = RecursionTopic.CalculateFactorial(input);
+
+            // Assert (Then)
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CalculateFactorial_GivenNegativeNumber_WhenCalled_ThenThrowsArgumentOutOfRangeException()
+        {
+            // Arrange (Given)
+            var input = -1;
+
+            // Act (When)
+            Action act = () => RecursionTopic.CalculateFactorial(input);
+
+            // Assert (Then)
+            Assert.Throws<ArgumentOutOfRangeException>(act);
+        }
+
+        // ===================== FIBONACCI =====================
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(5, 5)]
+        [InlineData(10, 55)]
+        public void CalculateFibonacci_GivenValidNumber_WhenCalled_ThenReturnsCorrectFibonacci(
+            long input, long expected)
+        {
+            // Arrange (Given)
+
+            // Act (When)
+            var result = RecursionTopic.CalculateFibonacci(input);
+
+            // Assert (Then)
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CalculateFibonacci_GivenNegativeNumber_WhenCalled_ThenThrowsArgumentOutOfRangeException()
+        {
+            // Arrange (Given)
+            var input = -5;
+
+            // Act (When)
+            Action act = () => RecursionTopic.CalculateFibonacci(input);
+
+            // Assert (Then)
+            Assert.Throws<ArgumentOutOfRangeException>(act);
+        }
+
+        [Fact]
+        public void CalculateFibonacci_GivenCachedValue_WhenCalledAgain_ThenUsesMemoizedResult()
+        {
+            // Arrange (Given)
+            var input = 20;
+            var firstCallResult = RecursionTopic.CalculateFibonacci(input);
+            var memoCountAfterFirstCall = RecursionTopic.memo.Count;
+
+            // Act (When)
+            var secondCallResult = RecursionTopic.CalculateFibonacci(input);
+
+            // Assert (Then)
+            Assert.Equal(firstCallResult, secondCallResult);
+            Assert.Equal(memoCountAfterFirstCall, RecursionTopic.memo.Count);
+        }
+    }
+}
---
+++ MyApp.Console/RecursionTopic/RecursionTopic.cs
@@ -0,0 +1,35 @@
+﻿namespace MyApp.Console.RecursionTopic
+{
+    public class RecursionTopic
+    {
+
+        //time O(n) - space O(n)
+        public static long CalculateFactorial(int num)
+        {
+            if (num < 0)
+                throw new ArgumentOutOfRangeException(nameof(num));
+
+
+            if (num == 0 || num == 1) return 1;
+
+            return num * CalculateFactorial(num - 1);
+        }
+
+        public static Dictionary<long, long> memo = new();
+        public static long CalculateFibonacci(long num)
+        {
+            if (num < 0) throw new ArgumentOutOfRangeException(nameof(num));
+
+            if (num <= 1) return num;
+
+            if (memo.ContainsKey(num))
+            {
+                return memo[num];
+            }
+
+            memo[num] = CalculateFibonacci(num - 1) + CalculateFibonacci(num - 2);
+
+            return memo[num];
+        }
+    }
+}
